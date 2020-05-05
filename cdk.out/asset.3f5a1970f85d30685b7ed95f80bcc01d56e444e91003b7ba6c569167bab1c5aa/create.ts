23c7618a49cced8df2c6655e6a2285c15eee387b
Georg Ludewig <georg.ludewig@mobiliar.ch>
2020-05-05 17:02:53 +0200

    ongoing testing

--- cdk.out/asset.3f5a1970f85d30685b7ed95f80bcc01d56e444e91003b7ba6c569167bab1c5aa/create.ts
+++ cdk.out/asset.3f5a1970f85d30685b7ed95f80bcc01d56e444e91003b7ba6c569167bab1c5aa/create.ts
@@ -1,10 +1,11 @@
+import { DynamoDB } from 'aws-sdk';
 import {DocumentClient} from "aws-sdk/lib/dynamodb/document_client";
-import {v4} from "uuid";
 
 const TABLE_NAME = process.env.TABLE_NAME || '';
 const PRIMARY_KEY = process.env.PRIMARY_KEY || '';
 
-const db = new DocumentClient();
+const db = new DynamoDB.DocumentClient();
+const uuidv4 = require('uuid/v4');
 
 const RESERVED_RESPONSE = `Error: You're using AWS reserved keywords as attributes`,
   DYNAMODB_EXECUTION_ERROR = `Error: Execution update, caused a Dynamodb error, please take a look at your CloudWatch Logs.`;
@@ -15,7 +16,7 @@ export const handler = async (event: any = {}) : Promise <any> => {
     return { statusCode: 400, body: 'invalid request, you are missing the parameter body' };
   }
   const item = typeof event.body == 'object' ? event.body : JSON.parse(event.body);
-  item[PRIMARY_KEY] = v4();
+  item[PRIMARY_KEY] = uuidv4();
   const params:DocumentClient.PutItemInput = {
     TableName: TABLE_NAME,
     Item: item
--- index.ts
+++ index.ts
@@ -1,14 +1,46 @@
 import {IResource, LambdaIntegration, MockIntegration, PassthroughBehavior, RestApi} from '@aws-cdk/aws-apigateway';
 import {App, RemovalPolicy, Stack, Tag} from '@aws-cdk/core';
 import {AttributeType, Table} from '@aws-cdk/aws-dynamodb';
-import {AssetCode, Runtime, Function} from '@aws-cdk/aws-lambda';
+import {AssetCode, Function, Runtime} from '@aws-cdk/aws-lambda';
 
 export class CdkPocStack extends Stack {
   constructor(app: App, id: string) {
     super(app, id);
 
-    // DYNAMO DB
-    const dynamoTable = new Table(this, `${id}-items`, {
+    const dynamoTable = this.dynamoDb(id);
+
+    const {getOneLambda, getAllLambda, createOne, updateOne, deleteOne} = this.lambdas(dynamoTable);
+
+    this.apiGateway(id, getAllLambda, createOne, getOneLambda, updateOne, deleteOne);
+  }
+
+  private apiGateway(id: string, getAllLambda: Function, createOne: Function, getOneLambda: Function, updateOne: Function, deleteOne: Function) {
+    const api = new RestApi(this, `${id}-items-api`, {
+      restApiName: `${id}-items`
+    });
+
+    const items = api.root.addResource('items');
+    const getAllIntegration = new LambdaIntegration(getAllLambda);
+    items.addMethod('GET', getAllIntegration);
+
+    const createOneIntegration = new LambdaIntegration(createOne);
+    items.addMethod('POST', createOneIntegration);
+    this.addCorsOptions(items);
+
+    const singleItem = items.addResource('{id}');
+    const getOneIntegration = new LambdaIntegration(getOneLambda);
+    singleItem.addMethod('GET', getOneIntegration);
+
+    const updateOneIntegration = new LambdaIntegration(updateOne);
+    singleItem.addMethod('PATCH', updateOneIntegration);
+
+    const deleteOneIntegration = new LambdaIntegration(deleteOne);
+    singleItem.addMethod('DELETE', deleteOneIntegration);
+    this.addCorsOptions(singleItem);
+  }
+
+  private dynamoDb(id: string) {
+    return new Table(this, `${id}-items`, {
       partitionKey: {
         name: 'itemId',
         type: AttributeType.STRING
@@ -16,12 +48,13 @@ export class CdkPocStack extends Stack {
       tableName: `${id}-items`,
 
       // The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
-      // the new table, and it will remain in your account until manually deleted. By setting the policy to 
+      // the new table, and it will remain in your account until manually deleted. By setting the policy to
       // DESTROY, cdk destroy will delete the table (even if it has data in it)
       removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
     });
+  }
 
-    // LAMBDA
+  private lambdas(dynamoTable: Table) {
     const getOneLambda = new Function(this, 'getOneItemFunction', {
       code: new AssetCode('src'),
       handler: 'get-one.handler',
@@ -78,65 +111,44 @@ export class CdkPocStack extends Stack {
     dynamoTable.grantReadWriteData(createOne);
     dynamoTable.grantReadWriteData(updateOne);
     dynamoTable.grantReadWriteData(deleteOne);
-
-    // API GATEWAY
-    const api = new RestApi(this, `${id}-items-api`, {
-      restApiName: `${id}-items`
-    });
-
-    const items = api.root.addResource('items');
-    const getAllIntegration = new LambdaIntegration(getAllLambda);
-    items.addMethod('GET', getAllIntegration);
-
-    const createOneIntegration = new LambdaIntegration(createOne);
-    items.addMethod('POST', createOneIntegration);
-    addCorsOptions(items);
-
-    const singleItem = items.addResource('{id}');
-    const getOneIntegration = new LambdaIntegration(getOneLambda);
-    singleItem.addMethod('GET', getOneIntegration);
-
-    const updateOneIntegration = new LambdaIntegration(updateOne);
-    singleItem.addMethod('PATCH', updateOneIntegration);
-
-    const deleteOneIntegration = new LambdaIntegration(deleteOne);
-    singleItem.addMethod('DELETE', deleteOneIntegration);
-    addCorsOptions(singleItem);
+    return {getOneLambda, getAllLambda, createOne, updateOne, deleteOne};
   }
+
+  private addCorsOptions(apiResource: IResource) {
+    apiResource.addMethod('OPTIONS', new MockIntegration({
+      integrationResponses: [{
+        statusCode: '200',
+        responseParameters: {
+          'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+          'method.response.header.Access-Control-Allow-Origin': "'*'",
+          'method.response.header.Access-Control-Allow-Credentials': "'false'",
+          'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
+        },
+      }],
+      passthroughBehavior: PassthroughBehavior.NEVER,
+      requestTemplates: {
+        "application/json": "{\"statusCode\": 200}"
+      },
+    }), {
+      methodResponses: [{
+        statusCode: '200',
+        responseParameters: {
+          'method.response.header.Access-Control-Allow-Headers': true,
+          'method.response.header.Access-Control-Allow-Methods': true,
+          'method.response.header.Access-Control-Allow-Credentials': true,
+          'method.response.header.Access-Control-Allow-Origin': true,
+        },
+      }]
+    })
+  }
+
 }
 
-export function addCorsOptions(apiResource: IResource) {
-  apiResource.addMethod('OPTIONS', new MockIntegration({
-    integrationResponses: [{
-      statusCode: '200',
-      responseParameters: {
-        'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
-        'method.response.header.Access-Control-Allow-Origin': "'*'",
-        'method.response.header.Access-Control-Allow-Credentials': "'false'",
-        'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
-      },
-    }],
-    passthroughBehavior: PassthroughBehavior.NEVER,
-    requestTemplates: {
-      "application/json": "{\"statusCode\": 200}"
-    },
-  }), {
-    methodResponses: [{
-      statusCode: '200',
-      responseParameters: {
-        'method.response.header.Access-Control-Allow-Headers': true,
-        'method.response.header.Access-Control-Allow-Methods': true,
-        'method.response.header.Access-Control-Allow-Credentials': true,
-        'method.response.header.Access-Control-Allow-Origin': true,
-      },  
-    }]
-  })
-}
 
 const app = new App();
-// Tag.add(dev, "cdk-poc", "cdk-poc");
-// Tag.add(dev, "cdk-poc-dev", "cdk-poc-dev");
-// const dev = new CdkPocStack(app, 'cdk-poc-dev');
+const dev = new CdkPocStack(app, 'cdk-poc-dev');
+Tag.add(dev, "cdk-poc", "cdk-poc");
+Tag.add(dev, "cdk-poc-dev", "cdk-poc-dev");
 const prod = new CdkPocStack(app, 'cdk-poc-prod');
 Tag.add(prod, "cdk-poc", "cdk-poc");
 Tag.add(prod, "cdk-poc-prod", "cdk-poc-prod");
