4d168ce3f6d6ec7ccbe77ac15bb34683d6ed7b2a
fischermatte <fischermatte@posteo.net>
2020-06-08 23:31:41 +0200

    added apigw->nlb configuration

--- api-gateway-nlb-fargate-service/cdk/bin/run.ts
+++ api-gateway-nlb-fargate-service/cdk/bin/run.ts
@@ -5,3 +5,4 @@ import {MyStack} from "../lib/my-stack";
 
 const app = new cdk.App();
 new MyStack(app);
+app.synth();
--- api-gateway-nlb-fargate-service/cdk/lib/my-stack.ts
+++ api-gateway-nlb-fargate-service/cdk/lib/my-stack.ts
@@ -1,118 +1,123 @@
-
 import {Construct, Stack, StackProps} from '@aws-cdk/core';
 import {
-  AuthorizationType,
-  ConnectionType,
-  HttpIntegration,
-  IResource,
-  MethodLoggingLevel, MockIntegration, PassthroughBehavior,
-  RestApi, VpcLink
+    ConnectionType,
+    HttpIntegration,
+    IResource,
+    MethodLoggingLevel,
+    MockIntegration,
+    PassthroughBehavior,
+    RestApi,
+    VpcLink
 } from "@aws-cdk/aws-apigateway";
 import {NetworkLoadBalancedFargateService} from "@aws-cdk/aws-ecs-patterns";
 import {Cluster, ContainerImage, FargateTaskDefinition} from "@aws-cdk/aws-ecs";
 import {Port, Vpc} from "@aws-cdk/aws-ec2";
 
 export class MyStack extends Stack {
-  constructor(scope: Construct, config?: StackProps) {
-    super(scope, 'MyStack', config);
-    const service = this.setupService();
-    this.setupApiGw(service);
-  }
+    constructor(scope: Construct, config?: StackProps) {
+        super(scope, 'MyStack', config);
+        const service = this.setupService();
+        this.setupApiGw(service);
+    }
 
-  private setupService(): NetworkLoadBalancedFargateService {
-    const vpc = new Vpc(this, 'MyVpc');
+    private setupService(): NetworkLoadBalancedFargateService {
+        const vpc = new Vpc(this, 'MyVpc');
 
-    const cluster = new Cluster(this, 'MyCluster', {
-      vpc: vpc,
-    });
-    cluster.connections.allowFromAnyIpv4(Port.tcp(8080));
+        const cluster = new Cluster(this, 'MyCluster', {
+            vpc: vpc,
+        });
+        cluster.connections.allowFromAnyIpv4(Port.tcp(80));
 
-    const taskDefinition = new FargateTaskDefinition(this, 'MyTaskDefinition');
+        const taskDefinition = new FargateTaskDefinition(this, 'MyTaskDefinition');
 
-    const container = taskDefinition.addContainer('MyContainer', {
-      image: ContainerImage.fromRegistry('kornkitti/express-hello-world'),
-    });
-    container.addPortMappings({
-      containerPort: 8080,
-      hostPort: 8080,
-    });
+        const container = taskDefinition.addContainer('MyContainer', {
+            image: ContainerImage.fromRegistry('vad1mo/hello-world-rest'),
+        });
+        container.addPortMappings({
+            containerPort: 5050
+        });
 
-    const service = new NetworkLoadBalancedFargateService(this, 'MyFargateServie', {
-      cluster,
-      taskDefinition,
-      assignPublicIp: true,
-    });
+        const service = new NetworkLoadBalancedFargateService(this, 'MyFargateServie', {
+            cluster,
+            taskDefinition,
+            publicLoadBalancer: false,
+        });
 
-    service.service.connections.allowFromAnyIpv4(Port.tcp(8080));
+        service.service.connections.allowFromAnyIpv4(Port.tcp(80));
 
-    return service;
-  }
+        return service;
+    }
 
-  private setupApiGw(service: NetworkLoadBalancedFargateService) {
-    // vpc link in order to allow access to NLB
-    const vpcLink = new VpcLink(this, 'MyVpcLink', {
-      targets: [service.loadBalancer],
-      vpcLinkName: 'MyVpcLink',
-    });
-
-    const api = new RestApi(this, `MyApi`, {
-      restApiName: `MyApi`,
-      deployOptions: {
-        loggingLevel: MethodLoggingLevel.INFO,
-      },
-    });
-    api.root.addProxy({
-      anyMethod: true,
-      defaultIntegration: new HttpIntegration('http://localhost.com:8080', {
-        httpMethod: 'ANY',
-        options: {
-          connectionType: ConnectionType.VPC_LINK,
-          vpcLink: vpcLink,
-        },
-        proxy: true,
-      }),
-      defaultMethodOptions: {
-        authorizationType: AuthorizationType.NONE,
-      },
-    });
-
-    this.addCorsOptions(api.root);
-  }
-
-  private addCorsOptions(apiResource: IResource) {
-    apiResource.addMethod(
-        'OPTIONS',
-        new MockIntegration({
-          integrationResponses: [
-            {
-              statusCode: '200',
-              responseParameters: {
-                'method.response.header.Access-Control-Allow-Headers':
-                    "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
-                'method.response.header.Access-Control-Allow-Origin': "'*'",
-                'method.response.header.Access-Control-Allow-Credentials': "'false'",
-                'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
-              },
+    private setupApiGw(service: NetworkLoadBalancedFargateService) {
+        const api = new RestApi(this, `MyApi`, {
+            restApiName: `MyApi`,
+            deployOptions: {
+                loggingLevel: MethodLoggingLevel.INFO,
             },
-          ],
-          passthroughBehavior: PassthroughBehavior.NEVER,
-          requestTemplates: {
-            'application/json': '{"statusCode": 200}',
-          },
-        }),
-        {
-          methodResponses: [
-            {
-              statusCode: '200',
-              responseParameters: {
-                'method.response.header.Access-Control-Allow-Headers': true,
-                'method.response.header.Access-Control-Allow-Methods': true,
-                'method.response.header.Access-Control-Allow-Credentials': true,
-                'method.response.header.Access-Control-Allow-Origin': true,
-              },
+        });
+        const resource = api.root.addResource('fargate');
+        resource.addMethod("ANY", new HttpIntegration('http://' + service.loadBalancer.loadBalancerDnsName, {
+            httpMethod: 'ANY',
+            options: {
+                connectionType: ConnectionType.VPC_LINK,
+                vpcLink: new VpcLink(this, 'MyVpcLink', {
+                    targets: [service.loadBalancer],
+                    vpcLinkName: 'MyVpcLink',
+                })
             },
-          ],
-        }
-    );
-  }
+            proxy: true,
+        }))
+
+        // const resource = api.root.addResource('fargate').addResource('{proxy+}');
+        // resource.addMethod("ANY", new HttpIntegration('http://' + service.loadBalancer.loadBalancerDnsName + "/{proxy}", {
+        //     httpMethod: 'ANY',
+        //     options: {
+        //         connectionType: ConnectionType.VPC_LINK,
+        //         vpcLink: new VpcLink(this, 'MyVpcLink', {
+        //             targets: [service.loadBalancer],
+        //             vpcLinkName: 'MyVpcLink',
+        //         })
+        //     },
+        //     proxy: true,
+        // }))
+
+        this.addCorsOptions(api.root);
+    }
+
+    private addCorsOptions(apiResource: IResource) {
+        apiResource.addMethod(
+            'OPTIONS',
+            new MockIntegration({
+                integrationResponses: [
+                    {
+                        statusCode: '200',
+                        responseParameters: {
+                            'method.response.header.Access-Control-Allow-Headers':
+                                "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+                            'method.response.header.Access-Control-Allow-Origin': "'*'",
+                            'method.response.header.Access-Control-Allow-Credentials': "'false'",
+                            'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
+                        },
+                    },
+                ],
+                passthroughBehavior: PassthroughBehavior.NEVER,
+                requestTemplates: {
+                    'application/json': '{"statusCode": 200}',
+                },
+            }),
+            {
+                methodResponses: [
+                    {
+                        statusCode: '200',
+                        responseParameters: {
+                            'method.response.header.Access-Control-Allow-Headers': true,
+                            'method.response.header.Access-Control-Allow-Methods': true,
+                            'method.response.header.Access-Control-Allow-Credentials': true,
+                            'method.response.header.Access-Control-Allow-Origin': true,
+                        },
+                    },
+                ],
+            }
+        );
+    }
 }
